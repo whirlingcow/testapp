bf6796a27ef554f4b788bf74832a99e671866a14
Sannie Jönsson <sannie_boy87@msn.com>
2015-01-09 17:38:31 +0000

    added the login-function and everything surrounding that area

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
   get 'users/new'
 
   root             'static_pages#home'
@@ -11,6 +13,12 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
 
   get 'signup'  => 'users#new'
+
+  get    'login'   => 'sessions#new'
+  
+  post   'login'   => 'sessions#create'
+  
+  delete 'logout'  => 'sessions#destroy'
 
   resources :users
 
