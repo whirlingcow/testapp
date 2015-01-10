55eb6d107355942f4cceb96b786d6d188f843e1b
Sannie Jönsson <sannie_boy87@msn.com>
2015-01-10 13:50:03 +0000

    addded activation controll and password reset

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150109200152) do
+ActiveRecord::Schema.define(version: 20150110131325) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.boolean  "admin",           default: false
+    t.boolean  "admin",                   default: false
+    t.string   "rails"
+    t.string   "generate"
+    t.string   "migration"
+    t.string   "add_activation_to_users"
+    t.string   "activation_digest"
+    t.boolean  "activated",               default: false
+    t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
